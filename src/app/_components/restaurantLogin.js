6c7bc1a61966b8ca9cbcfa7662b95c26a93b99ea
NarendraDeoSingh <singh.narendradeo@gmail.com>
2024-10-04 21:45:24 +0530

    Login Form Handling and Validation

--- src/app/_components/restaurantLogin.js
+++ src/app/_components/restaurantLogin.js
@@ -1,17 +1,45 @@
+import { useState } from "react";
+
 const RestaurantLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleLogin = () => {
+    if (!email || !password) {
+      setError(true);
+    } else {
+      setError(false);
+      console.log(email,password)
+    }
+  };
   return (
     <>
       <h3>Login Component</h3>
       <div>
         <div className="input-wrapper">
-          <input type="text" placeholder="Enter email id" className="input-field" />
+          <input
+            type="text"
+            placeholder="Enter email id"
+            className="input-field"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {error && !email && <span className="input-error">Please enter correct data</span>}
         </div>
         <div className="input-wrapper">
-          <input type="password" placeholder="Enter password" className="input-field" />
+          <input
+            type="password"
+            placeholder="Enter password"
+            className="input-field"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && !password && <span className="input-error">Please enter correct data</span>}
         </div>
 
         <div className="input-wrapper">
-          <button className="input-field">Login</button>
+          <button className="input-field" onClick={handleLogin}>
+            Login
+          </button>
         </div>
       </div>
     </>
