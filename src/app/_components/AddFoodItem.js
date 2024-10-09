6c8be864f0fa357dfc18142a60d98b5b3a12c4bf
NarendraDeoSingh <singh.narendradeo@gmail.com>
2024-10-10 01:00:40 +0530

    adding food items to mongoDB

--- src/app/_components/AddFoodItem.js
+++ src/app/_components/AddFoodItem.js
@@ -1,13 +1,33 @@
 import { useState } from "react";
 const AddFoodItems = () => {
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
-  const [path, setPath] = useState('');
-  const [description, setDescription] = useState('');
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [path, setPath] = useState("");
+  const [description, setDescription] = useState("");
 
-  const handleAddFoodItem=()=>{
-    console.log(name,price,path,description)
-  }
+  const handleAddFoodItem = async () => {
+    console.log(name, price, path, description);
+    let restro_id;
+    const restaurantData = JSON.parse(localStorage.getItem("restaurantUser"));
+    
+    if (restaurantData) {
+      restro_id = restaurantData.result._id;
+    }
+    let response = await fetch("http://localhost:3000/api/restaurant/foods", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        price,
+        img_path: path,
+        description,
+        restro_id,
+      }),
+    });
+    response = await response.json();
+    if (response.success) {
+      alert("Food item added");
+    }
+  };
   return (
     <div className="container">
       <h1>Add New Food Item</h1>
@@ -48,7 +68,9 @@ const AddFoodItems = () => {
         />
       </div>
       <div className="input-wrapper">
-        <button className="button" onClick={handleAddFoodItem}>Add Food Item</button>
+        <button className="button" onClick={handleAddFoodItem}>
+          Add Food Item
+        </button>
       </div>
     </div>
   );
---
+++ src/app/api/restaurant/foods/route.js
@@ -0,0 +1,16 @@
+import { connectionStr } from "@/app/lib/db";
+import { foodSchema } from "@/app/lib/foodsModel";
+import mongoose from "mongoose";
+
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const payload = await request.json();
+  await mongoose.connect(connectionStr, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const food =new foodSchema(payload)
+  const result = await food.save();
+  return NextResponse.json({ result, success: true });
+}
